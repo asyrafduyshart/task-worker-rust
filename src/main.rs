7049908edf83bef27bfde2d5c9c349ad7846165d
developer <developer@lidowin.com>
2023-06-12 12:50:26 +0000

    - add dot env check

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ mod config;
 mod handlers;
 mod router;
 
+use std::{
+    env,
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+};
+
 use crate::router::route;
 use hyper::{
     service::{make_service_fn, service_fn},
@@ -10,7 +15,18 @@ use hyper::{
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = ([127, 0, 0, 1], 8050).into();
+    dotenv::dotenv().ok();
+    // Retrieve the IP address from the environment variable "IP"
+    let ip_str = env::var("LISTEN_IP").unwrap_or_else(|_| String::from("127.0.0.1"));
+    let ip: Ipv4Addr = ip_str
+        .parse()
+        .expect("Listen IP must be in the format x.x.x.x");
+
+    // Retrieve the port from the environment variable "PORT"
+    let port_str = env::var("APP_PORT").unwrap_or_else(|_| String::from("8050"));
+    let port: u16 = port_str.parse().expect("PORT must be a number");
+
+    let addr = SocketAddr::new(IpAddr::V4(ip), port);
 
     let make_svc = make_service_fn(|_conn| async { Ok::<_, hyper::Error>(service_fn(route)) });
 
