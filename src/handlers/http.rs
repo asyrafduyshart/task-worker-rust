3a1b01e1129234eabf6eb8f2f413f64b9bac9afd
developer <developer@lidowin.com>
2023-06-19 07:04:50 +0000

    - add headers into http request

--- src/handlers/http.rs
+++ src/handlers/http.rs
@@ -1,18 +1,24 @@
-use std::time::Duration;
+use std::{collections::HashMap, time::Duration};
 
-use hyper::{client::HttpConnector, Body, Client, Request, Response, StatusCode, Uri};
+use hyper::{
+    client::HttpConnector,
+    http::{request, HeaderName, HeaderValue},
+    Body, Client, Request, Response, StatusCode, Uri,
+};
 use hyper_tls::HttpsConnector;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_json::to_string;
 use tokio::time::timeout;
 
 use super::response::ApiResponse;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug, Serialize)]
 pub struct HttpPostPayload {
     pub url: String,
     pub method: String,
     pub payload: String,
+    // key value pairs from json
+    pub headers: Option<HashMap<String, String>>,
     pub timeout: u64,
 }
 
@@ -36,12 +42,23 @@ pub async fn handle(
 
     println!("uri: {:?}", uri);
 
-    let request = Request::builder()
+    let mut request = Request::builder()
         .method(post_payload.method.as_str())
         .header("Content-Type", "application/json")
         .uri(uri)
         .body(Body::from(post_payload.payload))
         .unwrap();
+
+    // add all headers from post_payload.headers into request using for loop
+    // check if post_payload.headers is None
+    if post_payload.headers.is_some() {
+        for (key, value) in post_payload.headers.unwrap() {
+            let header_key = HeaderName::from_bytes(key.as_bytes()).unwrap();
+            let header_value = HeaderValue::from_str(value.as_str()).unwrap();
+            // make into headers format
+            request.headers_mut().insert(&header_key, header_value);
+        }
+    }
 
     let response = client.request(request);
     let duration = Duration::from_secs(post_payload.timeout);
@@ -50,6 +67,9 @@ pub async fn handle(
     match response {
         Ok(resp) => match resp {
             Ok(_) => {
+                // print response body string
+                // let whole_body = hyper::body::to_bytes(res.into_body()).await?;
+                // println!("response body: {:?}", whole_body);
                 let response = ApiResponse::new("success", "Message sent successfully");
                 return Ok(Response::new(Body::from(to_string(&response).unwrap())));
             }
